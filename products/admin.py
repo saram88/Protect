5d4547182bd9a5385d26106ca057a40a8ea235c7
Sara Mentzer <sara_mentzer@hotmail.com>
2023-09-06 09:31:39 +0000

    created checkout app and models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.Register(Product)
-admin.site.Register(Category)
+# register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
