f720f397eb0e0991b087008bd5390a9a145db3a1
Sara Mentzer <sara_mentzer@hotmail.com>
2023-09-06 20:25:37 +0000

    Product details

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 
 # Create your views here.
 
@@ -13,3 +17,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
