37664bc1a1f1fab4779704dc4e99bae1fa8aebb0
Sara Mentzer <sara_mentzer@hotmail.com>
2024-01-15 15:59:14 +0000

    Added views for wishlist

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,4 +8,17 @@ urlpatterns = [
         views.order_history,
         name='order_history'
     ),
+    path(
+        'wishlist/', 
+        views.wishlist, 
+        name='wishlist'
+    ),
+    path('add_wishlist/<int:product_id>',
+        views.add_to_wishlist,
+        name='add_to_wishlist'
+    ),
+    path('remove_from_wishlist/<int:wishlist_id>',
+        views.remove_from_wishlist,
+        name='remove_from_wishlist'
+    )
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import UserProfile
+from .models import UserProfile, UserWishlist
 from .forms import UserProfileForm
 
 from checkout.models import Order
+from products.models import Product
 
 
 @login_required
@@ -52,3 +53,45 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def wishlist(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    wishlist = profile.wishlist.all()
+
+    template = 'profiles/wishlist.html'
+    context = {
+        'wishlist': wishlist,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def add_to_wishlist(request, product_id):
+
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, you have to log in to add a to wishlist.')
+        return redirect(reverse('products'))
+    
+    profile = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    
+    wished_product,created = UserWishlist.objects.get_or_create(
+        wished_product=product,
+        user_profile = profile,
+    )
+    messages.success(request,'The product was added to your wishlist')
+
+    return redirect(reverse('product_detail', args=[product.id]))
+
+
+@login_required
+def remove_from_wishlist(request, wishlist_id):
+    """ Delete a wishlist """
+
+    wishlist = get_object_or_404(UserWishlist, pk=wishlist_id)
+    wishlist.delete()
+    messages.success(request, 'Product removed from wishlist!')
+    return redirect(reverse('wishlist'))
